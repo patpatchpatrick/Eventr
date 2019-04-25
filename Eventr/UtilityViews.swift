e3b4f8246afbfbf52ca852c5bc89f736d7d5e5b5
Patrick Doyle <patrickdoyle30@gmail.com>
2019-04-25 16:19:20 -0700

    First commit

---
+++ Eventr/UtilityViews.swift
@@ -0,0 +1,20 @@
+//
+//  UtilityViews.swift
+//  Eventr
+//
+//  Created by Patrick Doyle on 4/25/19.
+//  Copyright © 2019 Patrick Doyle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HairlineView: UIView {
+    override func awakeFromNib() {
+        guard let backgroundColor = self.backgroundColor?.cgColor else { return }
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = (1.0 / UIScreen.main.scale) / 2;
+        self.backgroundColor = UIColor.clear
+    }
+}
